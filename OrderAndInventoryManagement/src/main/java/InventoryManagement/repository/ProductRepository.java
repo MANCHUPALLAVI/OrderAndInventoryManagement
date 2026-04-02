783833d345ff21030b5ff6dd361185e1063b28fe
MANCHU PALLAVI <146466816+MANCHUPALLAVI@users.noreply.github.com>
2026-04-02 22:04:04 +0530

    ProductRepository.java

--- OrderAndInventoryManagement/src/main/java/InventoryManagement/repository/ProductRepository.java
+++ OrderAndInventoryManagement/src/main/java/InventoryManagement/repository/ProductRepository.java
@@ -3,6 +3,7 @@ package InventoryManagement.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import InventoryManagement.entity.Product;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Integer>
 {
     // Custom methods (optional)
