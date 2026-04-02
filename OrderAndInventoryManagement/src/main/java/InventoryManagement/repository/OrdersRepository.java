75e2ca9a7d5aebe1ff8e0266e741f404a877a699
MANCHU PALLAVI <146466816+MANCHUPALLAVI@users.noreply.github.com>
2026-04-02 22:03:42 +0530

    OrdersRepository.java

--- OrderAndInventoryManagement/src/main/java/InventoryManagement/repository/OrdersRepository.java
+++ OrderAndInventoryManagement/src/main/java/InventoryManagement/repository/OrdersRepository.java
@@ -2,7 +2,7 @@ package InventoryManagement.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import InventoryManagement.entity.Orders;
-
+@Repository
 public interface OrdersRepository extends JpaRepository<Orders, Integer> 
 {
 
